406096a52465b8f2f5ca3374e64e852a6a803d97
Lucas Gladding <l.gladding@vehikl.com>
2023-11-13 19:26:56 -0500

    Changed to happy-dom to avoid errors.

--- pages/jobs/index.spec.ts
+++ pages/jobs/index.spec.ts
@@ -1,12 +1,9 @@
 import { describe, test, expect } from 'vitest'
-import { setup } from '@nuxt/test-utils'
 import { mount } from '@vue/test-utils'
 
 import JobsIndex from './index.vue'
 
-describe('JobsIndex', async () => {
-    await setup()
-
+describe('JobsIndex', () => {
     test('renders the component', () => {
         const wrapper = mount(JobsIndex)
         expect(wrapper.exists()).toBe(true)
